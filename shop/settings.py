32e6d94781643fca155d0d8db9c415574b486ddb
yegor <wzxv_@outlook.com>
2023-08-21 00:10:34 +0300

    Fix: correct the static path(only for debug(should change for production)

--- shop/settings.py
+++ shop/settings.py
@@ -109,10 +109,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / "static",
+    ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
